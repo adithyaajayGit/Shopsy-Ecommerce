c40a26df2111f083da396ac38e2529061104ae4f
Adithya Ajay <adithyaajay098@gmail.com>
2024-04-09 18:09:59 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:"Iphone 13",
+      category:"mobile",
+      description:"This is iphone 13",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLUUq55k27aO5PXkY7-OWWJ4Yz8HY_7EImrw-9EKDiQ&s"
+    },
+    {
+      name:"Redmi note 13",
+      category:"mobile",
+      description:"This is Redmi note 13",
+      image:"https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/s/y/5/-original-imagwu894yyhyyce.jpeg?q=90&crop=false-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLUUq55k27aO5PXkY7-OWWJ4Yz8HY_7EImrw-9EKDiQ&s"
+    },
+    {
+      name:"Oppo F17 pro",
+      category:"mobile",
+      description:"This is Oppo F17 pro",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeEXCbNMv6O-16mkkGLixn63G3UPq6gJwIAINInE3_Q&s.flixcart.com/image/850/1000/xif0q/mobile/s/y/5/-original-imagwu894yyhyyce.jpeg?q=90&crop=false-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLUUq55k27aO5PXkY7-OWWJ4Yz8HY_7EImrw-9EKDiQ&s"
+    },
+    {
+      name:"Samsung s23 ",
+      category:"mobile",
+      description:"This is samsung s23",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSokioDkjsWb64K8UW4JS8OXMeniLdyZ6Wvx6sUQcDt8Q&s://https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeEXCbNMv6O-16mkkGLixn63G3UPq6gJwIAINInE3_Q&s.flixcart.com/image/850/1000/xif0q/mobile/s/y/5/-original-imagwu894yyhyyce.jpeg?q=90&crop=false-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLUUq55k27aO5PXkY7-OWWJ4Yz8HY_7EImrw-9EKDiQ&s"
+    }
+    
+  ]
+  res.render('index', { products,admin:false });
+});
+
+module.exports = router;
