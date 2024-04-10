9592b417e9fb8e3d7f70432e85a0d92c7d0abbc7
Adithya Ajay <adithyaajay098@gmail.com>
2024-04-11 03:08:45 +0530

    user checked in cart

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers=require('../helpers/user-helpers')
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -48,5 +55,11 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
+router.get( '/cart',verifyLogin,(req,res)=>{
+  
+  res.render('user/cart')
+})
+
+
 
 module.exports = router;
