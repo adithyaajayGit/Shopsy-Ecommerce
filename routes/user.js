d0b2da2def4b3310d8c48c50b831b3478c52b32a
Adithya Ajay <adithyaajay098@gmail.com>
2024-04-19 22:57:06 +0530

    total amount added in cart page

--- routes/user.js
+++ routes/user.js
@@ -65,8 +65,9 @@ router.get('/logout',(req,res)=>{
 })
 router.get( '/cart', verifyLogin,async(req,res)=>{
   let products=await userHelpers.getCartProducts(req.session.user._id)
+  let totalValue=await userHelpers.getTotalAmount(req.session.user._id)
   console.log(products)
-  res.render('user/cart',{products,user:req.session.user})
+  res.render('user/cart',{products,user:req.session.user,totalValue})
 })
 
 router.get('/add-to-cart/:id', (req,res)=>{
@@ -97,7 +98,7 @@ router.post('/remove-product',(req,res)=>{
 
 router.get('/place-order',verifyLogin,async(req,res)=>{
   let total=await userHelpers.getTotalAmount(req.session.user._id)
-  res.render('user/place-order',{total})
+  res.render('user/place-order',{total,user:req.session.user})
 })
 
 
